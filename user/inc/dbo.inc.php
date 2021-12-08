fd06a23b2c3dd55eff808010830c37147f396b23
Sarthika Dhawan <sarthika15170@iiitd.ac.in>
2021-12-08 20:17:29 +0530

    Add files via upload

---
+++ user/inc/dbo.inc.php
@@ -0,0 +1,11 @@
+<?php 
+$db_servername = "ftp.sgp-21.host-webserver.com";
+$db_username = "apinet_bru";
+$db_password = "54QHbPiF";
+$db_name = "apinet_bru";
+
+
+date_default_timezone_set("Asia/Calcutta");
+global $conn;
+$conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
+?>
---
+++ user/inc/sample.php
@@ -0,0 +1,15 @@
+<?php
+include "db.inc.php";
+
+$sql = "SELECT * FROM users";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+?>
